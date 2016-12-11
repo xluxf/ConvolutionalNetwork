20620d58831616f1760795164e3d50afbd800a24
xluxf <lux.filip@gmail.com>
2016-12-11 16:09:39 +0100

    FCLayer_update

---
+++ FCLayer.cpp
@@ -0,0 +1,106 @@
+//
+// Created by Filip Lux on 24.11.16.
+//
+
+#include <iostream>
+#include <vector>
+
+#include "FCLayer.h"
+
+
+
+// fullConnected layer
+FCLayer::FCLayer(int &inputs, int &neurons, Layer* lower) { //creates layer, number of inputs and neurons
+    n = neurons;
+    in = inputs;
+    down = lower;
+    input = down->out;
+    down->ou = n;
+    ddot = new double[n];
+    out = new double[n];
+    w = new double[in*n];
+    bias = new double[n];
+
+
+
+    for (int i = 0; i < in*n; ++i) { //randomly initializes weights
+        w[i] = fRand(INIT_MIN,INIT_MAX);
+    }
+    for (int i = 0; i < n; ++i) { //randomly initializes weights
+        bias[i] = fRand(INIT_MIN,INIT_MAX);
+    }
+}
+
+FCLayer::FCLayer(int &inputs, int &neurons) {
+    n = neurons;
+    in = inputs;
+    ddot = new double[n];
+    out = new double[n];
+    w = new double[in*n];
+    bias = new double[n];
+
+    for (int i = 0; i < in*n; ++i) { //randomly initializes weights
+        w[i] = fRand(INIT_MIN,INIT_MAX);
+    }
+    for (int i = 0; i < n; ++i) { //randomly initializes weights
+        bias[i] = fRand(INIT_MIN,INIT_MAX);
+    }
+
+};
+
+FCLayer::~FCLayer() {
+    delete bias;
+    delete ddot;
+    delete out;
+    delete w;
+};
+
+void FCLayer::forward_layer() { //step forward with activation function
+    for (int i = 0; i < n; i++) {
+        out[i] = bias[i];
+        for (int j = 0; j < in; j++) {
+            out[i] += w[i*in+j] * input[j];
+        }
+        out[i] = sigma(out[i]);
+    }
+}
+
+void FCLayer::backProp_layer() {
+    for (int i = 0; i < n; i++) {
+        ddot[i] = 0;
+        for (int j = 0; j < ou; j++) {
+           ddot[i] += up_ddot[j] * up_w[i+j*ou];
+        }
+    }
+    FCLayer::learn();
+}
+
+void FCLayer::backProp_layer(std::vector <double> result) {
+    for (int i = 0; i < n; i++) {
+        ddot[i] = (out[i] - result[i]) * out[i] * (1-out[i]);
+    }
+    FCLayer::learn();
+
+}
+
+void FCLayer::print() {
+    std::cout << "layer weights:" << std::endl;
+    for (int i = 0; i < n*n+1; i++) {
+        std::cout <<  "w" << i << ": " << w[i] << ", ";
+    }
+    std::cout << std::endl;
+}
+
+void FCLayer::learn() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j<in; j++) {
+            w[i*in+j] -= ddot[i] * input[j] * LR;
+        }
+    }
+}
+
+void FCLayer::update_input(double* in) {
+    input = in;
+};
+
+
---
+++ FCLayer.h
@@ -0,0 +1,83 @@
+//
+// Created by Filip Lux on 24.11.16.
+//
+
+#ifndef NN_NEURON_FCLAYER_H
+#define NN_NEURON_FCLAYER_H
+
+#include "Layer.h"
+
+/**
+*@brief FullConnected layer
+*/
+class FCLayer : public Layer{
+public:
+
+    double* ddot;
+    double* out;
+    double* w;
+
+    double* input;
+    double* up_out;
+    double* up_w;
+    double* up_ddot;
+    double* down_ddot;
+
+
+    /**
+    *@brief constructor for the first layer
+    *@param inputs number of inputs
+    *@param neurons number of neurons
+    */
+    FCLayer(int &inputs, int &neurons);
+
+    /**
+    *@brief constructor for the upper layers
+    *@param inputs number of inputs
+    *@param neurons number of neurons
+    *@param lower lower layer
+    */
+    FCLayer(int &inputs, int &neurons, Layer* lower);
+
+    /**
+    *@brief forward
+    */
+    void forward_layer();
+
+    /**
+    * @brief backpropagation
+    */
+
+    void backProp_layer();
+
+    /**
+    * @brief backpropagation for last layer
+    * @param result vector of expecting answer
+    */
+    void backProp_layer(std::vector <double> result);
+
+    /**
+    * @brief print weights
+    */
+
+    void learn();
+    /**
+    * @brief unsert new values to input
+    */
+    void update_input(double* in);
+
+    /**
+    * @brief print
+    */
+    void print();
+
+    /**
+    * @brief destructor
+    */
+    ~FCLayer();
+
+
+};
+
+
+#endif //NN_NEURON_FCLAYER_H

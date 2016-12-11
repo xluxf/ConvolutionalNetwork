7aaa59ac84c8c15414bebe5096f946dacf2babaf
Filip Lux <xlux@fi.muni.cz>
2016-12-11 15:37:10 +0100

    Pooling Layer

--- ConvolutionalNetwork/ConvolutionalNetwork/PoolLayer.h
+++ ConvolutionalNetwork/ConvolutionalNetwork/PoolLayer.h
@@ -6,8 +6,55 @@
 #define NN_NEURON_POOLLAYER_H
 
 
-class PoolLayer {
+#include <vector>
+#include "Layer.h"
 
+class PoolLayer : public Layer{
+
+
+    /**
+     * Constructor
+     * @param lower pointer to lower layer
+     */
+    PoolLayer(Layer* lower);
+
+    /**
+     * Destructor
+     * @param lower pointer to lower layer
+     */
+    ~PoolLayer();
+
+
+    /**
+     * @brief forward pass
+     */
+    void forward_layer();
+
+    /**
+    * @brief backpropagation
+    */
+    void backProp_layer();
+
+    /**
+    * @brief backpropagation for last layer
+    * @param result vector of expecting answer
+    */
+    void backProp_layer(std::vector <double> result);
+
+
+    /**
+    * @brief changing weights according ddot
+    */
+    void learn();
+    /**
+    * @brief insert new values to input
+    */
+    void update_input(double* in);
+
+    /**
+    * @brief print
+    */
+    void print();
 };
 
 
